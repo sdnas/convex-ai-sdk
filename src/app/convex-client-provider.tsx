effebbb450b020b5490a1550d88aa0f701235b0e
duasosandy <sansd.dev@gmail.com>
2025-11-12 10:16:46 +0800

    fix function name

--- src/app/convex-client-provider.tsx
+++ src/app/convex-client-provider.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function ConvextClientProvider({ children }: { children: ReactNode }) {
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
     <ConvexAuthNextjsProvider client={convex}>
       {children}
